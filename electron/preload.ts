8bb2d019686245db3d3d9782501f6bbc1acecfdd
Duttonn <natao.dutton@gmail.com>
2026-02-05 14:59:11 +0100

    feat: refine UI aesthetic, implement project-thread selection, and upgrade composer layout

---
+++ electron/preload.ts
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+  clone: (repoUrl, targetDir) => ipcRenderer.invoke('fs:clone', { repoUrl, targetDir }),
+  read: (filePath) => ipcRenderer.invoke('fs:read', filePath),
+  write: (filePath, content) => ipcRenderer.invoke('fs:write', { filePath, content }),
+  getNativeContext: () => {
+    return new Promise((resolve) => {
+      ipcRenderer.once('native-context-reply', (event, data) => resolve(data));
+      ipcRenderer.send('get-native-context');
+    });
+  }
+});
