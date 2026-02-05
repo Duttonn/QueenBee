f2a6e37987749ed6d7795db319d3035991aaa12b
Duttonn <natao.dutton@gmail.com>
2026-02-05 17:37:04 +0100

    feat(electron): expose fs, shell and git APIs in preload (TASK-05)

--- electron/preload.ts
+++ electron/preload.ts
@@ -4,6 +4,23 @@ contextBridge.exposeInMainWorld('electron', {
   clone: (repoUrl, targetDir) => ipcRenderer.invoke('fs:clone', { repoUrl, targetDir }),
   read: (filePath) => ipcRenderer.invoke('fs:read', filePath),
   write: (filePath, content) => ipcRenderer.invoke('fs:write', { filePath, content }),
+  
+  fs: {
+    readFile: (filePath) => ipcRenderer.invoke('fs:read', filePath),
+    writeFile: (filePath, content) => ipcRenderer.invoke('fs:write', { filePath, content }),
+    readDir: (dirPath) => ipcRenderer.invoke('fs:readDir', dirPath),
+  },
+
+  shell: {
+    openExternal: (url) => ipcRenderer.invoke('shell:openExternal', url),
+    showItemInFolder: (filePath) => ipcRenderer.invoke('shell:showItemInFolder', filePath),
+  },
+
+  git: {
+    status: (projectPath) => ipcRenderer.invoke('git:status', projectPath),
+    diff: (projectPath, filePath) => ipcRenderer.invoke('git:diff', { projectPath, filePath }),
+  },
+
   getNativeContext: () => {
     return new Promise((resolve) => {
       ipcRenderer.once('native-context-reply', (event, data) => resolve(data));
